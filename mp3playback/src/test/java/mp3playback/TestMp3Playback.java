500af03096333069be35739eb42b816e7ad12adc
George Marinellis <g.marinellis@adacom.net>
2014-02-05 11:53:34 +0200

    [no issue]

--- mp3playback/src/test/java/mp3playback/TestMp3Playback.java
+++ mp3playback/src/test/java/mp3playback/TestMp3Playback.java
@@ -9,7 +9,6 @@ public class TestMp3Playback {
 	private static Scanner in;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.out.println("Hello World!");
 
